b734959e9ff073f7de358dbb733d2db6279c522c
1VinceP <vpalmergraphics@gmail.com>
2018-10-05 10:04:50 -0600

    update sprite and platforms

--- src/LittleGame.js
+++ src/LittleGame.js
@@ -4,6 +4,7 @@ import Phaser from 'phaser';
 class LittleGame extends Component {
     game = {};
     bg = '';
+    cursors = {};
 
     // Build the Game class
     componentDidMount() {
@@ -13,26 +14,27 @@ class LittleGame extends Component {
         const renderOptions = {
             width,
             height,
-            renderer: Phaser.WEBGL,
+            renderer: Phaser.AUTO,
             parent: 'render-game',
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: { y: 200 }
+                    gravity: { y: 300 }
                 }
             },
             scene: {
                 preload: this.preload,
-                create: this.create
+                create: this.create,
+                update: this.update
             }
         };
-        this.game = new Phaser.Game( renderOptions );
+        this.game = new Phaser.Game(renderOptions);
     };
 
     // Load assets
     preload() {
-        this.load.image( 'sky', 'assets/sky.png' );
-        this.load.image( 'cat', 'assets/cat.png' );
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('cat', 'assets/cat.png');
     };
 
     // Create the game
@@ -43,20 +45,57 @@ class LittleGame extends Component {
             scale: { start: 1, end: 0 },
             blendMode: 'ADD'
         });
-        // Set and scale the background image
-        this.bg = this.add.image( this.game.config.width / 2, this.game.config.height / 2, 'sky' );
-        this.bg.setDisplaySize( this.game.config.width, this.game.config.height );
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.bg = this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'sky'); // Set and scale the background image
+        this.bg.setDisplaySize(this.game.config.width, this.game.config.height); // Make background size of screen
+        this.bg.setDepth(-1); // Send background to very back
+        this.input.on('pointerdown', () => console.log('Click!!'))
 
         // Create the cat and make it move
         // The numbers define where it starts
-        const cat = this.physics.add.sprite( 100, 100, 'cat' );
-        cat.setDisplaySize( 75, 50, true )
-        cat.setVelocity( 100, 200 );
-        cat.setBounce( 1, 1 );
-        cat.setCollideWorldBounds( true );
+        this.cat = this.physics.add.sprite(500, 0, 'cat');
+        this.cat.setDisplaySize(75, 50, true)
+        // this.cat.setVelocity( 100, 100 );
+        // this.cat.setBounce( 1, 1 );
+        this.cat.setCollideWorldBounds(true);
+        emitter.startFollow(this.cat);
 
-        emitter.startFollow( cat );
+        //Create Platforms
+        this.platforms = this.physics.add.group({
+            collideWorldBounds: true,
+            allowGravity: false,
+            immovable: true,
+            velocityX: 100,
+        });
+        this.platforms.create(200, 164, 'cat');
+        this.platforms.create(400, 280, 'cat');
+        this.platforms.create(600, 396, 'cat');
+        this.platforms.create(800, 512, 'cat');
+
+        // Create collision between cat and platforms
+        this.physics.add.collider(this.cat, this.platforms)
     };
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.cat.setVelocityX(-160);
+        }
+        else if (this.cursors.right.isDown) {
+            this.cat.setVelocityX(160);
+        }
+        else if( this.cursors.down.isDown ) {
+            this.cat.setVelocityY(330)
+        }
+        else {
+            this.cat.setVelocityX(0);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.cat.setVelocityY(-330);
+        }
+    }
 
     render() {
         return <div id='render-game' />
